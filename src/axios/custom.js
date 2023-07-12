24bd0c161901702948a40325279dd00df190f3f0
Mykhaylo Maslyuk <myhamike@gmail.com>
2023-07-12 19:27:58 +0300

    new changes

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -2,14 +2,14 @@ import React from 'react';
 import axios from 'axios';
 
 const authFetch = axios.create({
-    baseURL: 'https://songappmyhamike.onrender.com',
+    baseURL: 'https://songappserverhak.onrender.com',
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
 }
 );
 
-const urlAuth = 'https://songappmyhamike.onrender.com/auth';
+const urlAuth = 'https://songappserverhak.onrender.com/auth';
 
 export {
     authFetch,
