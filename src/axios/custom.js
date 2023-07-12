3000ec728546429641113b3dd6670ae584c7f7ed
Mykhaylo Maslyuk <myhamike@gmail.com>
2023-07-12 20:28:00 +0300

    newwest

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -2,14 +2,14 @@ import React from 'react';
 import axios from 'axios';
 
 const authFetch = axios.create({
-    baseURL: 'https://songappserverhak.onrender.com',
+    baseURL: 'https://songapphak.onrender.com',
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
 }
 );
 
-const urlAuth = 'https://songappserverhak.onrender.com/auth';
+const urlAuth = 'https://songapphak.onrender.com/auth';
 
 export {
     authFetch,
