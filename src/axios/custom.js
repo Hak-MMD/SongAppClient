bcbdcda05e17b4948f2ce6ca16650ee14a977817
Mykhaylo Maslyuk <myhamike@gmail.com>
2023-07-13 09:05:44 +0300

    simple fixes 2

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 const authFetch = axios.create({
-    baseURL: 'https://songapphak.onrender.com',
+    baseURL: 'https://songapphak.onrender.com/api',
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
